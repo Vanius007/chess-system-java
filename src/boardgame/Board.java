ce12df4f509d852eff8583b81a638879b8a78ebb
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-03-26 20:32:18 -0300

    Colocando as peças no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,18 +1,14 @@
 package boardgame;
 
-public class Board { 
-	
+public class Board {
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
-	
-	
+
 	public Board() {
-		
+
 	}
-
-
 
 	public Board(int rows, int columns) {
 		this.rows = rows;
@@ -20,44 +16,34 @@ public class Board {
 		pieces = new Piece[rows][columns];
 	}
 
-
-
 	public int getRows() {
 		return rows;
 	}
-
-
 
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
 
-
-
 	public int getColumns() {
 		return columns;
 	}
 
-
-
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
-	
+
 	public Piece piece(int row, int column) {
 		return pieces[row][column];
 	}
-	
-	
+
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 
+	public void placePiece(Piece piece, Position position) {
 
-
-	
-	
-	
-	
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 
 }
