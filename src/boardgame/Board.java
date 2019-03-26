5fa3494202537de3cd7b612a4b5a2e09de539bf8
Vanius Silva Oliveira <vaniusmax@gmail.com>
2019-03-26 20:12:24 -0300

    camada do xadrez e imprimindo o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -14,7 +14,7 @@ public class Board {
 
 
 
-	public Board(int rows, int columns, Piece[][] piece) {
+	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -42,6 +42,15 @@ public class Board {
 
 	public void setColumns(int columns) {
 		this.columns = columns;
+	}
+	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
 	}
 
 
